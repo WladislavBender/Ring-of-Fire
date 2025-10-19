ca83ba4b4efc23ebe4d535ab28f42eed36e61333
Wladislav Bender <wladislav.bender@gmail.com>
2025-10-19 18:27:46 +0200

    added first cards to game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent {}
 
-}
